eafbfe94fee2bbc67aa480fccd61db722a5dd231
Rhema <reymarmcoc@gmail.com>
2026-02-07 02:21:04 +0100

    Blue button

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function CTA({ name = "", color = "" }) {
   return (
     <button
-      className={`text-[16px] px-6 py-3 font-[body] text-(--white) bg-(--${color}) rounded-md `}
+      className="text-[16px] px-6 py-3 font-[body] text-(--white) rounded-md"
+      style={{
+        backgroundColor: `var(--${color})`
+      }}
     >
       {name}
     </button>
